4b626e40716d45718af669b0074a3ef542d5e64a
Georgi Georgiev <sinystr@gmail.com>
2017-08-19 10:03:35 +0300

    Add seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+User.create( email: 'admin', 
+             password: '$2a$10$zKBLJubRnM1yVDFFxRgGlulFwR8IlSsU9ayu7KF0lYJk.mc9c9V06', 
+             name: 'Admin Admin',
+             rank: 1,
+             secret_answer: 'Fight club',
+             password_salt: '$2a$10$zKBLJubRnM1yVDFFxRgGlu' )
